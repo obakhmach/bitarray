660bc1750c553746628fb37373ed00cb5a89e7e0
oleksander <alexandrbakhmach@gmail.com>
2022-12-25 03:40:17 +0200

    Add commends. Update README.md.

--- src/lib.rs
+++ src/lib.rs
@@ -2,13 +2,12 @@
 
 use std::fmt;
 
+/// A constant to describe the number of bits in one byte.
+/// Defined just to improve readability.
 const ONE_BYTE_BITS_COUNT: i8 = 8;
 
-#[derive(Debug)]
-pub struct BitArray {
-    pub size: i64,
-    pub bit_array: Vec<u8>,
-}
+/// Struct represents a custom error that should be raised every time
+/// a user tries to access or set the bit in the array that is out of this bit array size.
 #[derive(Clone, Debug)]
 pub struct OutOfRangeError {
     pub bitarray_size: i64,
@@ -16,6 +15,9 @@ pub struct OutOfRangeError {
 }
 
 impl OutOfRangeError {
+    /// Constructor used to initialize a new OutOfRangeError with a given bitarray_size and bitarray_position.
+    /// "bitarray_size" - The size in bits of the bitarray where the error was raised.
+    /// "bitarray_position" - The wrong position caused an error.
     pub fn new(bitarray_size: i64, bitarray_position: i64) -> Self {
         Self {
             bitarray_size,
@@ -34,11 +36,40 @@ impl fmt::Display for OutOfRangeError {
     }
 }
 
+/// A structure aimed to bring the bitarray functionality.
+/// Structure is described within two fields.
+/// "size" - The number of bits that will be allocated during the
+///          struct instance initialization.
+/// "bit_array" - The vector of 8 bit integer used to represent bits
+///               where each 8 bits are packed in every 8 bit integer.
+/// ```rust
+/// use bitarray::BitArray;
+///
+/// let bitarray_size: i64 = 9999;
+///
+/// let mut bitarray: BitArray = BitArray::new(bitarray_size);
+///
+/// bitarray.set(12, true).unwrap();
+///
+/// assert_eq!(bitarray.get(12).unwrap(), true);
+/// ```
+#[derive(Debug)]
+pub struct BitArray {
+    pub size: i64,
+    pub bit_array: Vec<u8>,
+}
+
 impl BitArray {
+    /// Constructor used to initialize a new instance of the bitarray with a given size.
+    /// "size" - The number of bits that will be allocated during the
+    ///          struct instance initialization.
     pub fn new(size: i64) -> Self {
+        // Calculates the number of elements should be allocated in vector per given size
         let _capacity: usize = (size / ONE_BYTE_BITS_COUNT as i64) as usize + 1;
+
         let mut bit_array: Vec<u8> = Vec::with_capacity(_capacity);
 
+        // Initialize vector with default 0 values. Where each value equal to 8 false bits.
         for _ in 0.._capacity {
             bit_array.push(0);
         }
@@ -46,16 +77,21 @@ impl BitArray {
         Self { size, bit_array }
     }
 
+    /// Calculates the element in bit array vector should be picked per given bit position.
+    /// In other words, calculates the index in bitarray vector.
     fn calc_vec_position(position: i64) -> usize {
         (position / ONE_BYTE_BITS_COUNT as i64) as usize
     }
 
+    /// Calculates the bit offset (position) in a given 8 bit integer.
+    /// The position should be counted from right to left.
     fn calc_byte_offset(position: i64) -> u8 {
         let _pow: i64 = position % ONE_BYTE_BITS_COUNT as i64;
 
         2u64.pow(_pow as u32) as u8
     }
 
+    /// Sets either true or false value in bit array at given position.
     pub fn set(&mut self, position: i64, flag: bool) -> Result<(), OutOfRangeError> {
         if position >= self.size {
             Err(OutOfRangeError::new(self.size, position as i64))
@@ -73,6 +109,7 @@ impl BitArray {
         }
     }
 
+    /// Gets either true or false value in bit array at given position.
     pub fn get(&self, position: i64) -> Result<bool, OutOfRangeError> {
         if position >= self.size {
             Err(OutOfRangeError::new(self.size, position))
